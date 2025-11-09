1d3d02d8946e056332b1df7bbca13fe20b037022
Trần Anh Bảo <trananhbao555@gmail.com>
2025-11-09 12:46:31 +0700

    Commit Change

--- src/main/java/com/example/demo/config/SecurityConfiguration.java
+++ src/main/java/com/example/demo/config/SecurityConfiguration.java
@@ -54,6 +54,8 @@ public class SecurityConfiguration {
                         .requestMatchers(HttpMethod.GET,"/api/v1/companies/**").permitAll()
                         .requestMatchers(HttpMethod.GET,"/api/v1/jobs/**").permitAll()
                         .requestMatchers(HttpMethod.GET,"/api/v1/skills/**").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/api/v1/submit-cv").authenticated()
+                        .requestMatchers(HttpMethod.GET,"/api/v1/cvs/**").authenticated()
                         .anyRequest().authenticated()
                 )
 
